39ce4a3fb37a881d469fcd1dc68e83905e9adff3
proofer <1290776+proofer@users.noreply.github.com>
2024-07-20 17:02:19 -0700

    TAPPING_TERM to 300

--- keyboards/ferris/sweep/keymaps/proofer/keymap.c
+++ keyboards/ferris/sweep/keymaps/proofer/keymap.c
@@ -32,25 +32,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,  KC_LABK,   KC_RABK,   KC_UNDS,   KC_QUES,   /*|*/ KC_AMPR,   KC_LPRN,   KC_LCBR,   KC_LBRC,   KC_PERC,
     KC_EXLM,   KC_MINUS,  KC_PLUS,   KC_EQUAL,  KC_HASH,   /*|*/ KC_PIPE,   OS_SHFT,   OS_CMD,    OS_ALT,    OS_CTRL,
     KC_CIRC,   KC_AT,     KC_ASTR,   KC_BSLS,   KC_DLR,    /*|*/ KC_TILD,   KC_RPRN,   KC_RCBR,   KC_RBRC,   KC_SLASH,
-           ______,               _______,                  /*|*/        _______,            _______
+           _______,               _______,                  /*|*/        _______,            _______
   ),
   [NAV] = LAYOUT_split_3x5_2(
-    _______,   _______,   _______,   _______,   _______,   /*|*/ _______,   _______,   KC_PGUP,   _______,   _______,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   /*|*/ XXXXXXX,   XXXXXXX,   KC_PGUP,   XXXXXXX,   XXXXXXX,
     OS_CTRL,   OS_ALT,    OS_CMD,    OS_SHFT,   KC_DELETE, /*|*/ KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  SW_NEXT,
-    _______,   _______,   _______,   _______,   CW_TOGG,   /*|*/ _______,   KC_PGDN,   _______,   _______,   _______,
-           ______,               _______,                  /*|*/        _______,            _______
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   CW_TOGG,   /*|*/ XXXXXXX,   KC_PGDN,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+           _______,               _______,                  /*|*/        _______,            _______
   ),
   [NUM] = LAYOUT_split_3x5_2(
     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     /*|*/ KC_F11,    KC_7,      KC_8,      KC_9,      KC_BSPC,
     OS_CTRL,   OS_ALT,    OS_CMD,    OS_SHFT,   TO(0),     /*|*/ KC_0,      KC_4,      KC_5,      KC_6,      KC_MINUS,
     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    /*|*/ KC_F12,    KC_1,      KC_2,      KC_3,      KC_DOT,
-           ______,               _______,                  /*|*/        _______,            _______
+           _______,               _______,                  /*|*/        _______,            _______
   ),
   [MODS] = LAYOUT_split_3x5_2(
-    C(KC_MB1), A(KC_MB1), G(KC_MB1), S(KC_MB1), _______,   /*|*/ _______,   _______,   _______,   _______,   QK_BOOT,
-    KC_LCTL,   KC_LALT,   KC_LCMD,   KC_LSFT,   TO(0),     /*|*/ _______,   _______,   _______,   _______,   _______,
-    _______,   _______,   _______,   _______,   _______,   /*|*/ _______,   _______,   _______,   _______,   _______,
-           ______,               _______,                  /*|*/        _______,            _______
+    C(KC_MB1), A(KC_MB1), G(KC_MB1), S(KC_MB1), XXXXXXX,   /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   QK_BOOT,
+    KC_LCTL,   KC_LALT,   KC_LCMD,   KC_LSFT,   TO(0),     /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+           _______,               _______,                  /*|*/        _______,            _______
   ),
 };
 
--- keyboards/ferris/sweep/keymaps/proofer/rules.mk
+++ keyboards/ferris/sweep/keymaps/proofer/rules.mk
@@ -1,5 +1,4 @@
 SPLIT_KEYBOARD = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
-SRC += oneshot.c
 SRC += swapper.c
