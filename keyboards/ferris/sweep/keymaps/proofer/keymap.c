c1a4008e38c7016702884a38f17331e51a2566c1
proofer <1290776+proofer@users.noreply.github.com>
2024-07-22 07:32:48 -0700

    tri-layer: allow releasing one layer key

--- keyboards/ferris/sweep/keymaps/proofer/keymap.c
+++ keyboards/ferris/sweep/keymaps/proofer/keymap.c
@@ -7,13 +7,14 @@ enum layers {
     SYM,  // Symbols
     NAV,  // Navigation
     NUM,  // Numbers & Fn keys, n in 1..12
-    MODS, // Mod keys & Reset
+    MISC, // Mouse keys & Reset
 };
 
 enum keycodes {
     SW_NEXT = SAFE_RANGE, // Switch to next desktop (cmd-tab)
 };
 
+// keycode macros
 #define OS_SHFT OSM(MOD_LSFT)
 #define OS_CTRL OSM(MOD_LCTL)
 #define OS_ALT OSM(MOD_LALT)
@@ -43,13 +44,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [NUM] = LAYOUT_split_3x5_2(
     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     /*|*/ KC_F11,    KC_7,      KC_8,      KC_9,      KC_BSPC,
-    OS_CTRL,   OS_ALT,    OS_CMD,    OS_SHFT,   TO(BASE),  /*|*/ KC_0,      KC_4,      KC_5,      KC_6,      KC_MINUS,
+    OS_CTRL,   OS_ALT,    OS_CMD,    OS_SHFT,   KC_COMMA,  /*|*/ KC_0,      KC_4,      KC_5,      KC_6,      KC_MINUS,
     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    /*|*/ KC_F12,    KC_1,      KC_2,      KC_3,      KC_DOT,
                        _______,          _______,          /*|*/        _______,          _______
   ),
-  [MODS] = LAYOUT_split_3x5_2(
+  [MISC] = LAYOUT_split_3x5_2(
     C(KC_MB1), A(KC_MB1), G(KC_MB1), S(KC_MB1), XXXXXXX,   /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   QK_BOOT,
-    KC_LCTL,   KC_LALT,   KC_LCMD,   KC_LSFT,   TO(0),     /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   TO(BASE),  /*|*/ XXXXXXX,   XXXXXXX,   KC_ENTER,  XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
                        _______,          _______,          /*|*/        _______,          _______
   ),
@@ -64,7 +65,7 @@ const uint16_t PROGMEM combo5[] = { KC_V, KC_B, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo0, KC_ESCAPE),
-    COMBO(combo1, TO(MODS)),
+    COMBO(combo1, TO(MISC)),
     COMBO(combo2, LCMD(KC_Z)),
     COMBO(combo3, LCMD(KC_X)),
     COMBO(combo4, LCMD(KC_C)),
@@ -72,11 +73,39 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 bool sw_desk_active = false;
+bool sym_pressed = false;
+bool nav_pressed = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     update_swapper(
         &sw_desk_active, KC_LCMD, KC_TAB, SW_NEXT,
         keycode, record
     );
+    switch ( keycode ) {
+        case TL_SYM:
+            if (record->event.pressed) {
+                sym_pressed = true;
+            } else {
+                sym_pressed = false;
+                layer_off(SYM);
+                if (nav_pressed) {
+                    return false;
+                }
+            }
+            return true;
+        case TL_NAV:
+            if (record->event.pressed) {
+                nav_pressed = true;
+            } else {
+                nav_pressed = false;
+                layer_off(NAV);
+                if (sym_pressed) {
+                    return false;
+                }
+            }
+            return true;
+        default:
+            return true;
+        };
     return true;
 }
