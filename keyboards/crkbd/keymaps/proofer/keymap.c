19203f4bb83a7c1d4a09d9db66177139ec831561
proofer <1290776+proofer@users.noreply.github.com>
2024-07-24 12:26:27 -0700

    swap NAV/LSFT,SYM/SPC thumb keys

--- keyboards/crkbd/keymaps/proofer/keymap.c
+++ keyboards/crkbd/keymaps/proofer/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      /*|*/ KC_Y,      KC_U,     KC_I,       KC_O,      KC_P,
     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      /*|*/ KC_H,      KC_J,     KC_K,       KC_L,      KC_SCLN,
     KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      /*|*/ KC_N,      KC_M,     KC_COMMA,   KC_DOT,    KC_QUOTE,
-                 KC_LCMD,     TL_NAV,      KC_LSFT,        /*|*/        KC_SPC,      TL_SYM,      KC_BSPC
+                 KC_LCMD,     KC_LSFT,     TL_NAV,         /*|*/        TL_SYM,      KC_SPC,      KC_BSPC
   ),
   [SYM] = LAYOUT_split_3x5_3(
     KC_GRAVE,  KC_LABK,   KC_RABK,   KC_UNDS,   KC_QUES,   /*|*/ KC_AMPR,   KC_LPRN,   KC_LCBR,   KC_LBRC,   KC_PERC,
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [NAV] = LAYOUT_split_3x5_3(
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   /*|*/ KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  XXXXXXX,
-    OS_SHFT,   OS_ALT,    OS_CMD,    OS_CTRL,   XXXXXXX,   /*|*/ KC_DEL,    KC_ENTER,  KC_TAB,    SW_NEXT,   XXXXXXX,
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   CW_TOGG,   /*|*/ XXXXXXX,   KC_PGDN,   KC_PGUP,   XXXXXXX,   XXXXXXX,
+    OS_SHFT,   OS_ALT,    OS_CMD,    OS_CTRL,   CW_TOGG,   /*|*/ KC_DEL,    KC_ENTER,  KC_TAB,    SW_NEXT,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   /*|*/ XXXXXXX,   KC_PGDN,   KC_PGUP,   XXXXXXX,   XXXXXXX,
                  _______,     _______,     _______,        /*|*/        _______,     _______,     _______
   ),
   [NUM] = LAYOUT_split_3x5_3(
