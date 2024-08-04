61976887d440a8244cc8fecfd9028f6803f97b12
proofer <1290776+proofer@users.noreply.github.com>
2024-08-04 05:17:42 -0700

    Sweep: fix typos in keyboard.c

--- keyboards/ferris/sweep/keymaps/proofer/keymap.c
+++ keyboards/ferris/sweep/keymaps/proofer/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      /*|*/ KC_Y,      KC_U,     KC_I,       KC_O,      KC_P,
     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      /*|*/ KC_H,      KC_J,     KC_K,       KC_L,      KC_SCLN,
     KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      /*|*/ KC_N,      KC_M,     KC_COMMA,   KC_DOT,    KC_QUOTE,
-                       TL_LSFT,          KC_NAV,           /*|*/        KC_SYM,           TL_SPC
+                       KC_LSFT,          TL_NAV,           /*|*/        TL_SYM,           KC_SPC
   ),
   [SYM] = LAYOUT_split_3x5_2(
     KC_GRAVE,  KC_LABK,   KC_RABK,   KC_UNDS,   KC_QUES,   /*|*/ KC_AMPR,   KC_LPRN,   KC_LCBR,   KC_LBRC,   KC_PERC,
