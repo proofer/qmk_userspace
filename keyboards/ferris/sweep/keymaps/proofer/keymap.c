f67b5ba5519b44b1ca6a9287000df239beb155c7
proofer <1290776+proofer@users.noreply.github.com>
2024-08-04 05:03:23 -0700

    Sweep: swap thumb keys both sides; replace/add combos

--- keyboards/ferris/sweep/keymaps/proofer/keymap.c
+++ keyboards/ferris/sweep/keymaps/proofer/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      /*|*/ KC_Y,      KC_U,     KC_I,       KC_O,      KC_P,
     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      /*|*/ KC_H,      KC_J,     KC_K,       KC_L,      KC_SCLN,
     KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      /*|*/ KC_N,      KC_M,     KC_COMMA,   KC_DOT,    KC_QUOTE,
-                       TL_NAV,           KC_LSFT,          /*|*/        KC_SPC,           TL_SYM
+                       TL_LSFT,          KC_NAV,           /*|*/        KC_SYM,           TL_SPC
   ),
   [SYM] = LAYOUT_split_3x5_2(
     KC_GRAVE,  KC_LABK,   KC_RABK,   KC_UNDS,   KC_QUES,   /*|*/ KC_AMPR,   KC_LPRN,   KC_LCBR,   KC_LBRC,   KC_PERC,
@@ -63,14 +63,16 @@ const uint16_t PROGMEM combo2[] = { KC_Z, KC_X, COMBO_END};
 const uint16_t PROGMEM combo3[] = { KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM combo4[] = { KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM combo5[] = { KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM combo6[] = { KC_S, KC_D, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo0, KC_ESCAPE),
     COMBO(combo1, TO(MISC)),
-    COMBO(combo2, LCMD(KC_Z)),
-    COMBO(combo3, LCMD(KC_X)),
+    COMBO(combo2, LCTL(KC_J)),
+    COMBO(combo3, LCTL(KC_X)),
     COMBO(combo4, LCMD(KC_C)),
     COMBO(combo5, LCMD(KC_V)),
+    COMBO(combo6, LCTL(KC_S)),
 };
 
 bool sw_desk_active = false;
