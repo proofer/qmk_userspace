eae5eef8763d725e3c4106ca630782bd9a9d0873
proofer <1290776+proofer@users.noreply.github.com>
2024-09-16 10:53:59 -0700

    all right pinky keys removed; "relaxed" tri-layer activation of FN

--- keyboards/brec34/keymaps/proofer/keymap3 b4-tri-layer.c
+++ keyboards/brec34/keymaps/proofer/keymap3 b4-tri-layer.c
@@ -1,6 +1,5 @@
-// brec34 & Ferris Sweep; right pinky column unused
+// brec34 & Ferris Sweep
 #include QMK_KEYBOARD_H
-#include "print.h"
 
 enum layers {
     BASE,    // Default
@@ -15,6 +14,8 @@ enum layers {
 #define OS_CTRL OSM(MOD_LCTL)
 #define OS_OPT OSM(MOD_LALT)
 #define OS_CMD OSM(MOD_LGUI)
+#define TL_SYM_P TL_LOWR
+#define TL_NUM_NAV TL_UPPR
 #define KC_MB1 KC_MS_BTN1
 
 // Shift-Space -> Backspace
@@ -29,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      /*|*/ KC_Y,      KC_U,     KC_I,       KC_O,      XXXXXXX,
     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      /*|*/ KC_H,      KC_J,     KC_K,       KC_L,      XXXXXXX,
     KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      /*|*/ KC_N,      KC_M,     KC_COMMA,   KC_DOT,    XXXXXXX,
-                       KC_LSFT,   LT(NUM_NAV, KC_ESC),  /*|*/     LT(SYM_P, KC_ENTER),   SPC_BSPC
+                       KC_LSFT,   LT(TL_NUM_NAV, KC_ESC)   /*|*/     LT(TL_SYM_P, KC_ENTER),   SPC_BSPC
   ),
   [SYM_P] = LAYOUT_split_3x5_2(
     KC_GRAVE,  KC_LABK,   KC_RABK,   KC_UNDS,   KC_QUES,   /*|*/ KC_AMPR,   KC_LPRN,   KC_LCBR,   KC_LBRC,   XXXXXXX,
@@ -38,20 +39,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        _______,          _______,          /*|*/        _______,          _______
   ),
   [NUM_NAV] = LAYOUT_split_3x5_2(
-    DB_TOGG,   XXXXXXX,   KC_UP,     CW_TOGG,   KC_PGUP,   /*|*/ KC_COMMA,  KC_7,      KC_8,      KC_9,      XXXXXXX,
-    XXXXXXX,   KC_HOME,   KC_END,    KC_TAB,    KC_DEL,    /*|*/ KC_0,      KC_1,      KC_2,      KC_3,      XXXXXXX,
+    XXXXXXX,   XXXXXXX,   KC_UP,     CW_TOGG,   KC_PGUP,   /*|*/ XXXXXXX,   KC_7,      KC_8,      KC_9,      XXXXXXXX,
+    XXXXXXX,   KC_HOME,   KC_END,    KC_TAB,    KC_DEL,    /*|*/ KC_0,      KC_1,      KC_2,      KC_3,      XXXXXXXX,
     XXXXXXX,   KC_LEFT,   KC_DOWN,   KC_RIGHT,  KC_PGDN,   /*|*/ KC_DOT,    KC_4,      KC_5,      KC_6,      XXXXXXX,
                        _______,          _______,          /*|*/        _______,          _______
   ),
   [FN] = LAYOUT_split_3x5_2(
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-    XXXXXXX,   KC_F1,     KC_F2,     KC_F3,     XXXXXXX,   /*|*/ XXXXXXX,   KC_F4,     KC_F5,     KC_F6,     XXXXXXX,
-    XXXXXXX,   KC_F7,     KC_F8,     KC_F9,     XXXXXXX,   /*|*/ XXXXXXX,   KC_F10,    KC_F11,    KC_F12,    XXXXXXX,
+    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     /*|*/ KC_F11,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   OS_OPT,    OS_CMD,    OS_CTRL,   XXXXXXX,   /*|*/ XXXXXXX,   OS_CTRL,   OS_CMD,    OS_OPT,    XXXXXXX,
+    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    /*|*/ KC_F12,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
                        _______,          _______,          /*|*/        _______,          _______
   ),
   [MISC] = LAYOUT_split_3x5_2(
     C(KC_MB1), A(KC_MB1), G(KC_MB1), S(KC_MB1), XXXXXXX,   /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   QK_BOOT,   XXXXXXX,
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   TO(BASE),  /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   TO(BASE),  /*|*/ XXXXXXX,   XXXXXXX,   KC_ENTER,  XXXXXXX,   XXXXXXX,
     QK_BOOT,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   /*|*/ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
                        _______,          _______,          /*|*/        _______,          _______
   ),
@@ -133,10 +134,39 @@ combo_t key_combos[] = {
     [jkl_R_CMD_CTL_OPT] = COMBO(jkl_combo, RCMD(RCTL(KC_ROPT))),
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // if FN ("adjust") on, leave it on until *both* SYM_P ("lower") and NUM_NAV ("upper") are off
-    if (state & ((layer_state_t)1 << FN)) {
-        if (state & (((layer_state_t)1 << SYM_P) | (((layer_state_t)1 << NUM_NAV)))) return state;
-    }
-    return update_tri_layer_state(state, SYM_P, NUM_NAV, FN);
+/*layer_state_t layer_state_set_user(layer_state_t state) {*/
+/*    return update_tri_layer_state(state, SYM_P, NUM_NAV, FN);*/
+/*}*/
+
+bool sym_p_pressed = false;
+bool num_nav_pressed = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch ( keycode ) {
+        case TL_SYM_P:
+            if (record->event.pressed) {
+                sym_p_pressed = true;
+            } else {
+                sym_p_pressed = false;
+                layer_off(SYM);
+                if (num_nav_pressed) {
+                    return false;
+                }
+            }
+            return true;
+        case TL_NUM_NAV:
+            if (record->event.pressed) {
+                num_nav_pressed = true;
+            } else {
+                num_nav_pressed = false;
+                layer_off(NAV);
+                if (sym_p_pressed) {
+                    return false;
+                }
+            }
+            return true;
+        default:
+            return true;
+        };
+    return true;
 }
